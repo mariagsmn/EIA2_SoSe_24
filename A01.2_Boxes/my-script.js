32b9130e156af96da6dd547b1bd684a2dfb98e19
mariagsmn <mariagassmann0@gmail.com>
2024-04-09 14:55:14 +0200

    A02

--- A01.2_Boxes/my-script.js
+++ A01.2_Boxes/my-script.js
@@ -4,35 +4,38 @@ var Boxes;
     let color;
     let x = 0;
     let y = 0;
+    // for-Schleife zur Erstellung der Boxen
     for (let i = 0; i < n; i++) {
         y += (i == 2) ? 20 : 50; // wenn y = 2 dann + 20, wenn nich 2, dann + 5
         x = (x + 170) % 400;
-        switch (i) {
+        switch (i) { // Festlegen der Farbe basierend auf dem Wert von i
             case 0:
-                color = "#ff0000";
+                color = "#ff0000"; // Rote Farbe für i == 0
                 break;
             case 1:
             case 4:
-                color = "#00ff00";
+                color = "#00ff00"; // Grüne Farbe für i == 1 oder i == 4
                 break;
             case 3:
-                continue;
+                continue; // Überspringen der aktuellen Iteration für i == 3
             default:
-                color = "#0000ff";
+                color = "#0000ff"; // Blaue Farbe für alle anderen Werte von i
         }
+        // Innere Schleife zur Erstellung verschiedener Größen
         for (let size of ["big", "medium", "small"]) {
-            createBox(color, x, y, size);
+            createBox(color, x, y, size); // Aufruf der Funktion zur Erstellung einer Box mit den Parametern
             if (i == 4) // wenn i GENAU 4 ist
-                break;
+                break; // Beenden der inneren Schleife vorzeitig
         }
     }
+    // Funktion zur Erstellung einer Box
     function createBox(_color, _x, _y, _size) {
-        let div = document.createElement("div");
-        document.body.appendChild(div);
-        div.classList.add(_size);
-        div.style.backgroundColor = _color;
-        div.style.left = _x + "px";
-        div.style.top = _y + "px";
+        let div = document.createElement("div"); // Erstellen eines <div>-Elements
+        document.body.appendChild(div); // Hinzufügen des <div>-Elements zum Body des Dokuments
+        div.classList.add(_size); // Hinzufügen einer Klasse entsprechend der Größe der Box
+        div.style.backgroundColor = _color; // Festlegen der Hintergrundfarbe der Box
+        div.style.left = _x + "px"; // Festlegen der linken Position der Box
+        div.style.top = _y + "px"; // Festlegen der oberen Position der Box
     }
 })(Boxes || (Boxes = {}));
 //# sourceMappingURL=my-script.js.map
---
+++ A02_EventInspector/my-script.ts
@@ -0,0 +1,22 @@
+let fixedspan: HTMLElement = document.querySelector(".fixed-span"); // HTML-Element, was verändert werden soll wird ausgewählt
+
+window.addEventListener("mousemove", moveSpan); // ganzes Fenster führt Funktion bei Mausbewegung aus
+window.addEventListener("click", logInfo);
+window.addEventListener("keyup", logInfo);
+
+
+function moveSpan(_event: MouseEvent): void { // Funktion wird erstellt
+    fixedspan.style.top = `${_event.clientY}px`; // auf zuvor ausgewähltes HTML wird zugegriffen, Style-Property (Inline-CSS) wird verändert - Top (Y-Achse)
+    fixedspan.style.left = `${_event.clientX}px`; // auf zuvor ausgewähltes HTML wird zugegriffen, Style-Property (Inline-CSS) wird verändert - Left (X-Achse)
+    logInfo(_event);
+    fixedspan.innerHTML = `x: ${_event.clientX} <br> y: ${_event.clientY}`; //ZUSÄTZLICH: Koordinaten werden in fixedspan geschrieben
+}
+
+function logInfo(_event: Event): void { // Funktion wird erstellt
+    console.log(_event.target);
+    console.log(_event.type);
+    console.log(_event.currentTarget);
+    console.log(_event);    
+}
+
+// `$ macht, dass Zahlenwerte in einen String (dynamisch) eingefügt werden können

a493b5967489714ca750e933c0ff6221f0a6a970
mariagsmn <mariagassmann0@gmail.com>
2024-05-25 10:42:58 +0200

    A09.1

--- A09.02/my-script.js
+++ A09.02/my-script.js
@@ -1,24 +1,76 @@
 var A09_02;
 (function (A09_02) {
+    class Clouds {
+        x;
+        y;
+        radius;
+        color;
+        constructor(_x, _y, _radius, _color) {
+            this.x = _x;
+            this.y = _y;
+            this.radius = _radius;
+            this.color = _color;
+        }
+        draw(ctx) {
+            ctx.save();
+            ctx.beginPath();
+            ctx.translate(this.x, this.y);
+            ctx.arc(15, -80, 15, 0, Math.PI * 2);
+            ctx.fillStyle = "white";
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
+        }
+        move(ctx) {
+            this.x = this.x + 0.2;
+            this.draw(ctx);
+        }
+    }
+    class Duck {
+        positionX;
+        positionY;
+        color;
+        constructor(_positionX, _positionY, _color) {
+            this.positionX = _positionX;
+            this.positionY = _positionY;
+            this.color = _color;
+        }
+        move(ctx) {
+            this.positionX = this.positionX + 1;
+            this.draw(ctx);
+        }
+        moveToLeft(ctx) {
+            this.positionX = this.positionX - 1;
+            this.draw(ctx);
+        }
+        draw(ctx) {
+            ctx.save();
+            ctx.beginPath();
+            ctx.translate(this.positionX, this.positionY);
+            ctx.fillStyle = this.color;
+            ctx.arc(10, 10, 10, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.ellipse(25, 28, 25, 20, 0, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
+        }
+    }
     window.addEventListener("load", () => {
         const canvas = document.getElementById("myCanvas");
         const ctx = canvas.getContext("2d");
-        // const sky = new Sky();
-        const cloud1 = new A09_02.Clouds(100, 100, 30, "white");
-        const cloud2 = new A09_02.Clouds(200, 150, 30, "white");
         const sun = new A09_02.Sun(canvas.width - 50, 50, 30); // Position oben rechts
         const mountains = new A09_02.Mountains();
+        const cloud = new Clouds(200, 80, 30, "white");
         const trees = new A09_02.Trees();
         const birds = new A09_02.Birds();
         const grass = new A09_02.Grass();
-        const lake = new A09_02.Lake(300, 200, 50, "blue");
+        const lake = new A09_02.Lake(300, 200, 80, "blue");
         const flyingInsects = new A09_02.FlyingInsects();
         function draw() {
             //    sky.draw(ctx);  //draw-Aufruf von dem Himmel
-            mountains.draw(ctx); //draw-Aufruf von den Bergen
             drawBackground();
-            cloud1.draw(ctx); //draw-Aufruf von den Wolke1
-            cloud2.draw(ctx); //draw-Aufruf von  Wolke2
+            mountains.draw(ctx); //draw-Aufruf von den Bergen
             sun.draw(ctx); //draw-Aufruf von der Sonne
             lake.draw(ctx); //draw-Aufruf vom See
             trees.draw(ctx); //draw-Aufruf von dem Baum
@@ -26,6 +78,43 @@ var A09_02;
             birds.animateFromLeftToRight(ctx, 20);
             grass.draw(ctx); //draw-Aufruf vom Gras
             flyingInsects.draw(ctx); //draw-Aufruf von den fliegenden Insekten   
+            let ducks = [];
+            let clouds = [];
+            let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            ducks.push(new Duck(100, 100, "yellow"));
+            ducks.push(new Duck(100, 50, "black"));
+            ducks.push(new Duck(40, 20, "red"));
+            clouds.push(new Clouds(100, 100, 30, "white"));
+            clouds.push(new Clouds(200, 105, 30, "white"));
+            function animation() {
+                ctx.putImageData(imgData, 0, 0);
+                for (let duck of ducks) {
+                    duck.move(ctx);
+                }
+                for (let cloud of clouds) {
+                    cloud.move(ctx);
+                }
+            }
+            function moveFromLeftToRight() {
+                ctx.putImageData(imgData, 0, 0);
+                for (let duck of ducks) {
+                    duck.moveToLeft(ctx);
+                }
+                for (let cloud of clouds) {
+                    cloud.draw(ctx);
+                }
+            }
+            const animateToRight = window.setInterval(function () {
+                animation();
+            }, 24);
+            window.setTimeout(() => {
+                clearInterval(animateToRight);
+            }, 3000); //nach 3 sek wird Code gestoppt (hört auf, sich nach rechts zu bewegen)
+            window.setTimeout(() => {
+                window.setInterval(() => {
+                    moveFromLeftToRight();
+                }, 24);
+            }, 7000); // nach 7 sek neuer Intervall
         }
         function drawBackground() {
             let gradient = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height);
@@ -36,47 +125,10 @@ var A09_02;
             ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         }
         draw();
-        class Duck {
-            positionX;
-            positionY;
-            color;
-            constructor(_positionX, _positionY, _color) {
-                this.positionX = _positionX;
-                this.positionY = _positionY;
-                this.color = _color;
-                this.draw();
-            }
-            move() {
-                this.positionX = this.positionX + 2;
-                this.draw();
-            }
-            draw() {
-                ctx.save();
-                ctx.beginPath();
-                ctx.translate(this.positionX, this.positionY);
-                ctx.fillStyle = this.color;
-                ctx.arc(10, 10, 10, 0, 2 * Math.PI);
-                ctx.fill();
-                ctx.ellipse(25, 28, 25, 20, 0, 0, 2 * Math.PI);
-                ctx.fill();
-                ctx.closePath();
-                ctx.restore();
-            }
-        }
-        // zeichenfunktion für tollen hintergrund
-        ctx.fillStyle = "lightblue";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.getImageData(0, 0, canvas.width, canvas.height); // später mit putImagedata alten canvas wieder holen
-        let ducks = [];
-        let imgData;
-        ducks.push(new Duck(100, 100, "yellow"));
-        ducks.push(new Duck(100, 50, "red"));
-        function animation() {
-            ctx.putImageData(imgData, 0, 0);
-            for (let duck of ducks) {
-                duck.move();
-            }
-        }
+        // zeichenfunktion für hintergrund
+        // ctx.fillStyle = "red";
+        // ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //    ctx.getImageData(0, 0, canvas.width, canvas.height); // später mit putImagedata alten canvas wieder holen
     });
 })(A09_02 || (A09_02 = {}));
 //# sourceMappingURL=my-script.js.map
--- A09.02/my-script.ts
+++ A09.02/my-script.ts
@@ -1,26 +1,93 @@
 namespace A09_02 {
 
+    class Clouds {
+        x: number;
+        y: number;
+        radius: number;
+        color: string;
+   
+       constructor(_x: number, _y: number, _radius: number, _color: string) {
+           this.x = _x;
+           this.y = _y;
+           this.radius = _radius;
+           this.color = _color;
+       }
+
+       draw(ctx: CanvasRenderingContext2D) {
+           ctx.save();
+           ctx.beginPath();
+           ctx.translate(this.x, this.y);
+           ctx.arc(15, -80, 15, 0, Math.PI * 2);
+           ctx.fillStyle = "white";
+           ctx.fill();
+           ctx.closePath();
+           ctx.restore();
+       }
+       
+       move(ctx: CanvasRenderingContext2D): void {
+        this.x = this.x + 0.2;
+        this.draw(ctx);
+    }
+       
+   }   
+
+   class Duck {
+    positionX: number;
+    positionY: number;
+    color: string;
+
+    constructor(_positionX: number, _positionY: number, _color: string) {
+        this.positionX = _positionX;
+        this.positionY = _positionY;
+        this.color = _color;
+    }
+
+    move(ctx: CanvasRenderingContext2D): void {
+        this.positionX = this.positionX + 1;
+        this.draw(ctx)
+    }
+
+    moveToLeft(ctx: CanvasRenderingContext2D): void {
+        this.positionX = this.positionX - 1;
+        this.draw(ctx)
+    }
+
+
+    draw(ctx: CanvasRenderingContext2D): void {
+        ctx.save();
+        ctx.beginPath();
+        ctx.translate(this.positionX, this.positionY);
+
+        ctx.fillStyle = this.color;
+        ctx.arc(10, 10, 10, 0, 2 * Math.PI);
+        ctx.fill();
+
+        ctx.ellipse(25, 28, 25, 20, 0, 0, 2 * Math.PI);
+        ctx.fill();
+
+        ctx.closePath();
+        ctx.restore();
+    }
+}
+
     window.addEventListener("load", () => {
         const canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
         const ctx = canvas.getContext("2d");
 
-        // const sky = new Sky();
-        const cloud1 = new Clouds(100, 100, 30, "white");
-        const cloud2 = new Clouds(200, 150, 30, "white");
         const sun = new Sun(canvas.width - 50, 50, 30); // Position oben rechts
         const mountains = new Mountains();
+        const cloud = new Clouds(200, 80, 30, "white");
         const trees = new Trees();
         const birds = new Birds();
         const grass = new Grass();
-        const lake = new Lake(300, 200, 50, "blue");
+        const lake = new Lake(300, 200, 80, "blue");
         const flyingInsects = new FlyingInsects();
 
         function draw() {
             //    sky.draw(ctx);  //draw-Aufruf von dem Himmel
-            mountains.draw(ctx);  //draw-Aufruf von den Bergen
             drawBackground();
-            cloud1.draw(ctx);  //draw-Aufruf von den Wolke1
-            cloud2.draw(ctx);  //draw-Aufruf von  Wolke2
+            mountains.draw(ctx);  //draw-Aufruf von den Bergen
+
             sun.draw(ctx);  //draw-Aufruf von der Sonne
             lake.draw(ctx);  //draw-Aufruf vom See
             trees.draw(ctx);  //draw-Aufruf von dem Baum
@@ -28,10 +95,62 @@ namespace A09_02 {
             birds.animateFromLeftToRight(ctx, 20);
             grass.draw(ctx); //draw-Aufruf vom Gras
             flyingInsects.draw(ctx);  //draw-Aufruf von den fliegenden Insekten   
+
+                    
+            let ducks: Duck[] = [];
+            let clouds: Clouds[] = [];
+            let imgData: ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            
+            ducks.push(new Duck(100, 100, "yellow"))
+            ducks.push(new Duck(100, 50, "black"))
+            ducks.push(new Duck(40, 20, "red"))
+
+            clouds.push(new Clouds(100, 100, 30, "white"))
+            clouds.push(new Clouds(200, 105, 30, "white"))
+
+            function animation(): void {
+                ctx.putImageData(imgData, 0, 0);
+
+                for (let duck of ducks) {
+                    duck.move(ctx);
+                }
+
+                for(let cloud of clouds) {
+                    cloud.move(ctx);
+                }
+            }
+
+            function moveFromLeftToRight(): void {
+                ctx.putImageData(imgData, 0, 0);
+
+                for (let duck of ducks) {
+                    duck.moveToLeft(ctx);
+                }
+
+                for (let cloud of clouds) {
+                    cloud.draw(ctx);
+                }
+
+                
+            }
+
+            const animateToRight = window.setInterval(function(): void{
+                animation();
+            }, 24)
+
+            window.setTimeout(() => {
+                clearInterval(animateToRight)
+            }, 3000)  //nach 3 sek wird Code gestoppt (hört auf, sich nach rechts zu bewegen)
+
+            window.setTimeout(() => {
+                window.setInterval(() => {
+                    moveFromLeftToRight();
+                }, 24)
+            }, 7000) // nach 7 sek neuer Intervall
+
         }
 
         function drawBackground(): void {
-
             let gradient: CanvasGradient = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height);
             gradient.addColorStop(0.1, "#2980b9");
             gradient.addColorStop(0.27, "orangered");
@@ -43,66 +162,16 @@ namespace A09_02 {
 
         draw();
 
-
-        class Duck {
-            positionX: number;
-            positionY: number;
-            color: string;
-
-            constructor(_positionX: number, _positionY: number, _color: string) {
-                this.positionX = _positionX;
-                this.positionY = _positionY;
-                this.color = _color;
-
-                this.draw()
-            }
-
-            move(): void {
-                    this.positionX = this.positionX + 2;
-        
-                    this.draw();
-                }
-            
-
-            draw(): void {
-                ctx.save();
-                ctx.beginPath();
-                ctx.translate(this.positionX, this.positionY);
-
-                ctx.fillStyle = this.color;
-                ctx.arc(10, 10, 10, 0, 2 * Math.PI);
-                ctx.fill();
-
-                ctx.ellipse(25, 28, 25, 20, 0, 0, 2 * Math.PI);
-                ctx.fill();
-
-                ctx.closePath();
-                ctx.restore();
-            }
-        }
-
-        // zeichenfunktion für tollen hintergrund
-        ctx.fillStyle = "lightblue";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.getImageData(0, 0, canvas.width, canvas.height); // später mit putImagedata alten canvas wieder holen
+        // zeichenfunktion für hintergrund
+        // ctx.fillStyle = "red";
+        // ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //    ctx.getImageData(0, 0, canvas.width, canvas.height); // später mit putImagedata alten canvas wieder holen
 
 
-        let ducks: Duck[] = [];
-        let imgData: ImageData;
-
-        ducks.push(new Duck(100, 100, "yellow"))
-        ducks.push(new Duck(100, 50, "red"))
-
-        function animation (): void{
-            ctx.putImageData(imgData, 0, 0);
-
-            for (let duck of ducks) {
-                duck.move();
-            }
-        }
-
-       
     });
+
+
+
 
 
 
